41c0e7d080e8edec2b4c596568025b2b6ff97118
Sierra555 <dara6234@gmail.com>
2024-10-26 21:36:11 +0300

    upd

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 $(function () {
-   
-  $('.top__slider').slick({
+  $('.js-hero-slider').slick({
     dots: true,
     arrows: false,
     fade: true,
@@ -9,18 +8,26 @@ $(function () {
     autoplay: true
   });
 
-  $('.new-collection__slider').slick({
+  $('.js-collection-slider').slick({
     arrows: false,
     slidesToShow: 5,
     slidesToScroll: 5,
   });
   
-  $('.new-collection__slider-clone').slick({
+  $('.js-collection-slider-v2').slick({
     arrows: false,
     dots: true,
     slidesToShow: 10,
     slidesToScroll: 5,
   });
+
+  $('.js-article-slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+
   var mixer = mixitup('.gallery__inner', {
     load: {
       filter: '.lvingroom'
@@ -28,12 +35,14 @@ $(function () {
   });
 
   const tel = document.getElementsByClassName('contacts__tel');
-  tel.style.color = "#5A5A5A";
+  
+  for (let i = 0; i < tel.length; i++) {
+    tel[i].style.color = "#5A5A5A";
+  }
 
   $('.article__slider').slick({
     slidesToScroll: 1,
     slidesToShow: 1,
     arrows: true,
   });
-  
 });
